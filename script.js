e9dbe99c98772af0379674c971a799d80603719d
alamir kamil <am4106611@gmail.com>
2026-09-09 15:38:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const darkBtn = document.getElementById("darkBtn");
-
-darkBtn.addEventListener("click", function () {
-
-    document.body.classList.toggle("dark");
-
-});
+const menuBtn=document.getElementById("menuBtn");const navLinks=document.getElementById("navLinks");menuBtn.addEventListener("click",()=>navLinks.classList.toggle("open"));document.querySelectorAll("#navLinks a").forEach(link=>link.addEventListener("click",()=>navLinks.classList.remove("open")));
